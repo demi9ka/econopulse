47241103d8e3916266c76bf7483ca427329884e9
Demid <107408661+demi9ka@users.noreply.github.com>
2023-12-30 21:11:32 +0200

    delete lolcalhost url

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:5200',
-    // baseURL: 'https://finstream-server.vercel.app',
+    // baseURL: 'http://localhost:5200',
+    baseURL: 'https://finstream-server.vercel.app',
 })
 
 export default api
