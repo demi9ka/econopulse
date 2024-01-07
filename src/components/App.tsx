20c3478e51c06c27cdae24a77b980ecea5864e4b
Demid <grazz1wmel@gmail.com>
2024-01-07 14:16:57 +0200

    add routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,30 +1,20 @@
 import styles from './app.module.css'
-import Calculator from './calculator/Calculator'
-import Chart from './chart/Chart'
-import EditMenu from './edit-menu/EditMenu'
 import Header from './header/Header'
-import { EditMenuProvider } from 'provider/EditMenuProvider'
-import { CalculationContext, ICalculationContext } from 'provider/CalculationProvider'
-import { useContext } from 'react'
-import LoadScreen from './load-screen/LoadScreen'
+
+import { Routes, Route } from 'react-router-dom'
+import Home from './home/Home'
+import Register from 'components/register/Register'
+import Login from 'components/login/Login'
 
 const App = () => {
-    const { INDEX_DATA } = useContext(CalculationContext) as ICalculationContext
-
     return (
         <div className={styles.container}>
             <Header />
-            {INDEX_DATA.length ? (
-                <>
-                    <Chart />
-                    <EditMenuProvider>
-                        <Calculator />
-                        <EditMenu />
-                    </EditMenuProvider>
-                </>
-            ) : (
-                <LoadScreen />
-            )}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
         </div>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,18 @@ import { CalculationProvider } from 'provider/CalculationProvider.tsx'
 import '@mantine/core/styles.css'
 import { MantineProvider } from '@mantine/core'
 import { ChartDataProvider } from 'provider/ChartDataProvider'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <MantineProvider defaultColorScheme="dark">
-            <CalculationProvider>
-                <ChartDataProvider>
-                    <App />
-                </ChartDataProvider>
-            </CalculationProvider>
-        </MantineProvider>
+        <BrowserRouter>
+            <MantineProvider defaultColorScheme="dark">
+                <CalculationProvider>
+                    <ChartDataProvider>
+                        <App />
+                    </ChartDataProvider>
+                </CalculationProvider>
+            </MantineProvider>
+        </BrowserRouter>
     </React.StrictMode>
 )
