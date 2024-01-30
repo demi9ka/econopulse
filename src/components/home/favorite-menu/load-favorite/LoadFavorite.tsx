f414374d9c6ec05ca2eef70124c9e142f74e7eaf
Demid <grazz1wmel@gmail.com>
2024-01-30 18:46:44 +0200

    add recovery block

--- src/components/home/favorite-menu/load-favorite/LoadFavorite.tsx
+++ src/components/home/favorite-menu/load-favorite/LoadFavorite.tsx
@@ -3,16 +3,17 @@ import { useContext, useState } from 'react'
 import { Loader } from '@mantine/core'
 import { FavoriteMenuContext, IFavoriteMenuContext } from 'provider/FavoriteProvider'
 import { CalculationContext, ICalculationContext } from 'provider/CalculationProvider'
-import { deleteFavorite } from 'services/favorite'
+import { createFavorite, deleteFavorite } from 'services/favorite'
 import { IconArrowsExchange, IconHexagonLetterR, IconX } from '@tabler/icons-react'
 import randomColor from 'utils/randomColor'
 import { cloneDeep } from 'lodash'
 
 const LoadFavorite = () => {
-    const { favorite_data, setFavoriteData, setViewModal } = useContext(FavoriteMenuContext) as IFavoriteMenuContext
+    const { favorite_data, setFavoriteData, setViewModal, cache_data, setCacheData } = useContext(FavoriteMenuContext) as IFavoriteMenuContext
     const { setCalculationData } = useContext(CalculationContext) as ICalculationContext
     const [error, setError] = useState('')
     const [active_item_id, setActiveItemId] = useState(-1)
+    const [action_state, setActionState] = useState<number[]>([])
     const setFavorite = () => {
         if (active_item_id < 0) return
         const active_item: any = favorite_data!.find(item => item.id == active_item_id)!.data
@@ -29,16 +30,41 @@ const LoadFavorite = () => {
     }
     const deleteItem = async (id: number) => {
         try {
+            setActionState(prev => [...prev, id])
             const res = await deleteFavorite(id)
             if (res.status == 200) {
+                setCacheData(prev => [...prev, favorite_data!.filter(item => item.id == res.data.id)[0]])
                 setFavoriteData(prev => prev!.filter(item => item.id !== res.data.id))
                 setActiveItemId(-1)
             }
         } catch (e: any) {
             setError(typeof e === 'string' ? e : 'Неудалось удалить')
+        } finally {
+            setActionState(prev => prev.filter(el => el !== id))
         }
     }
-
+    const recoveryItem = async (id: number) => {
+        const data = cache_data.filter(item => item.id === id)[0]
+        if (!data) {
+            setError('Не удалось восстановить')
+            return
+        }
+        const name = data.name
+        if (!data.data || !name) return
+        try {
+            setActionState(prev => [...prev, id])
+            const res = await createFavorite(name, data.data as any)
+            if (res.status == 201) {
+                res.data.data = JSON.parse(res.data.data)
+                setFavoriteData(prev => [...prev!, { ...res.data, create_date: new Date(res.data.create_date) }])
+                setCacheData(prev => prev.filter(el => el.id !== id))
+            } else throw false
+        } catch (e: any) {
+            setError(typeof e === 'string' ? e : 'Неудалось восстановить')
+        } finally {
+            setActionState(prev => prev.filter(el => el !== id))
+        }
+    }
     let favorite_content
 
     if (typeof favorite_data === 'undefined')
@@ -63,19 +89,53 @@ const LoadFavorite = () => {
                     {item.data.type == 'roc' && <IconHexagonLetterR width={18} color="#7950F2" />}
                     {item.data.type == 'overlay' && <IconArrowsExchange width={18} color="#FAB005" />}
                 </div>
-                <div className={styles.delete} onClick={() => deleteItem(item.id)}>
-                    <IconX color="#FA5252" width={17} height={17} />
-                </div>
+                {action_state.includes(item.id) ? (
+                    <Loader size={'xs'} />
+                ) : (
+                    <div className={styles.delete} onClick={() => deleteItem(item.id)}>
+                        <IconX color="#FA5252" width={17} height={17} />
+                    </div>
+                )}
             </div>
         ))
 
     return (
         <div className={styles.wrapper}>
-            <h2 className={styles.title}>
-                Сохранённые <span className={styles.count}>{typeof favorite_data === 'object' && favorite_data?.length}</span>
-                <span className={styles.error}>{!!error && error}</span>
-            </h2>
-            <div className={styles.favorite_wrapper}>{favorite_content}</div>
+            <div className={styles.content_wrapper}>
+                <h2 className={styles.title}>
+                    Сохранённые <span className={styles.count}>{typeof favorite_data === 'object' && favorite_data?.length}</span>
+                    <span className={styles.error}>{!!error && error}</span>
+                </h2>
+                <div className={styles.favorite_wrapper}>{favorite_content}</div>
+                {cache_data.length > 0 && (
+                    <>
+                        <h2 className={styles.title}>
+                            Корзина{' '}
+                            <span onClick={() => setCacheData([])} className={styles.clear_cache}>
+                                Очистить
+                            </span>
+                        </h2>
+                        <div className={styles.favorite_wrapper}>
+                            {cache_data!.map(item => (
+                                <div key={item.id} className={styles.item} style={{ opacity: 0.8 }}>
+                                    <div className={styles.name_wrapper}>
+                                        <p className={styles.name}>{item.name}</p>
+                                        {item.data.type == 'roc' && <IconHexagonLetterR width={18} color="#7950F2" />}
+                                        {item.data.type == 'overlay' && <IconArrowsExchange width={18} color="#FAB005" />}
+                                    </div>
+                                    {action_state.includes(item.id) ? (
+                                        <Loader size={'xs'} />
+                                    ) : (
+                                        <p className={styles.recovery} onClick={() => recoveryItem(item.id)}>
+                                            Восстановить
+                                        </p>
+                                    )}
+                                </div>
+                            ))}
+                        </div>
+                    </>
+                )}
+            </div>
             <button disabled={active_item_id === -1} className={styles.confirm_btn} onClick={setFavorite}>
                 Загрузить
             </button>
--- src/components/home/favorite-menu/save-favorite/SaveFavorite.tsx
+++ src/components/home/favorite-menu/save-favorite/SaveFavorite.tsx
@@ -4,23 +4,24 @@ import { createFavorite } from 'services/favorite'
 import { CalculationContext, ICalculationContext } from 'provider/CalculationProvider'
 import { FavoriteMenuContext, IFavoriteMenuContext } from 'provider/FavoriteProvider'
 import { IUserDataContext, UserDataContext } from 'provider/UserProvider'
-import { IconDeviceFloppy } from '@tabler/icons-react'
+import { Loader } from '@mantine/core'
 
 const SaveFavorite = () => {
     const [error, setError] = useState('')
     const [name, setName] = useState('')
-
+    const [req_proccess, setReqProccess] = useState(false)
     const { setFavoriteData, favorite_data } = useContext(FavoriteMenuContext) as IFavoriteMenuContext
     const { calculation_data } = useContext(CalculationContext) as ICalculationContext
     const { user_data } = useContext(UserDataContext) as IUserDataContext
     useEffect(() => {
         if (typeof favorite_data !== 'object') return
     }, [favorite_data])
-    const create = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
+    const create = async (event?: React.FormEvent<HTMLFormElement>) => {
+        event && event.preventDefault()
         setError('')
         if (!calculation_data || !user_data || typeof favorite_data !== 'object' || !name) return
         try {
+            setReqProccess(true)
             const res = await createFavorite(name, calculation_data)
             if (res.status == 201) {
                 res.data.data = JSON.parse(res.data.data)
@@ -29,6 +30,8 @@ const SaveFavorite = () => {
             } else throw false
         } catch (e: any) {
             setError(typeof e === 'string' ? e : 'Неудалось сохранить')
+        } finally {
+            setReqProccess(false)
         }
     }
     useEffect(() => {
@@ -43,8 +46,8 @@ const SaveFavorite = () => {
             <p className={styles.title}>Сохранить конфигурацию. {error && <span className={styles.error}>{error}</span>}</p>
             <form onSubmit={create}>
                 <input required className={styles.name_input} autoComplete="off" name="name" type="text" placeholder="Название" value={name} onChange={e => setName(e.target.value)} autoFocus={true} />
-                <button className={styles.submit_btn} disabled={name === '' || !calculation_data || !user_data || favorite_data == null || !!favorite_data.find(item => item.name === name.trim())} type="submit">
-                    <IconDeviceFloppy />
+                <button className={styles.submit_btn} disabled={name === '' || !calculation_data || !user_data || favorite_data == null || !!favorite_data.find(item => item.name === name.trim()) || req_proccess} type="submit">
+                    {req_proccess ? <Loader color="white" size={'xs'} /> : 'Сохранить'}
                 </button>
             </form>
         </div>
