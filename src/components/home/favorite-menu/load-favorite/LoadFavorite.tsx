100d31da35aefe280d4f5b80e410517921ccb69d
Demid <grazz1wmel@gmail.com>
2024-01-30 16:55:18 +0200

    remove date in favorite item

--- src/components/home/favorite-menu/load-favorite/LoadFavorite.tsx
+++ src/components/home/favorite-menu/load-favorite/LoadFavorite.tsx
@@ -8,8 +8,6 @@ import { IconArrowsExchange, IconHexagonLetterR, IconX } from '@tabler/icons-rea
 import randomColor from 'utils/randomColor'
 import { cloneDeep } from 'lodash'
 
-import moment from 'moment'
-
 const LoadFavorite = () => {
     const { favorite_data, setFavoriteData, setViewModal } = useContext(FavoriteMenuContext) as IFavoriteMenuContext
     const { setCalculationData } = useContext(CalculationContext) as ICalculationContext
@@ -52,49 +50,24 @@ const LoadFavorite = () => {
     else if (typeof favorite_data === null) favorite_content = <p className={styles.msg}>Ошибка загрузки</p>
     else if (favorite_data?.length == 0) favorite_content = <p className={styles.msg}>Нету сохранённых конфигураций</p>
     else if (favorite_data)
-        favorite_content = favorite_data!.map(item => {
-            const current_date = moment(new Date())
-            const create_date = moment(new Date(item.create_date))
-            const duration = moment.duration(current_date.diff(create_date))
-
-            let pref = 'д'
-            let diff = parseInt(String(duration.asDays()))
-            if (diff < 1) {
-                pref = 'ч'
-                diff = parseInt(String(duration.asHours()))
-            }
-            if (diff < 1) {
-                pref = 'мин'
-                diff = parseInt(String(duration.asMinutes()))
-            }
-            if (diff < 1) {
-                pref = 'сейчас'
-                diff = 0
-            }
-
-            return (
-                <div
-                    key={item.id}
-                    className={`${styles.item} ${item.id === active_item_id && styles.active}`}
-                    onClick={() => {
-                        setActiveItemId(item.id)
-                    }}
-                >
-                    <div className={styles.name_wrapper}>
-                        <p className={styles.date}>
-                            {diff > 0 && diff}
-                            {pref}
-                        </p>
-                        <p className={styles.name}>{item.name}</p>
-                        {item.data.type == 'roc' && <IconHexagonLetterR width={18} color="#7950F2" />}
-                        {item.data.type == 'overlay' && <IconArrowsExchange width={18} color="#FAB005" />}
-                    </div>
-                    <div className={styles.delete} onClick={() => deleteItem(item.id)}>
-                        <IconX color="#FA5252" width={17} height={17} />
-                    </div>
+        favorite_content = favorite_data!.map(item => (
+            <div
+                key={item.id}
+                className={`${styles.item} ${item.id === active_item_id && styles.active}`}
+                onClick={() => {
+                    setActiveItemId(item.id)
+                }}
+            >
+                <div className={styles.name_wrapper}>
+                    <p className={styles.name}>{item.name}</p>
+                    {item.data.type == 'roc' && <IconHexagonLetterR width={18} color="#7950F2" />}
+                    {item.data.type == 'overlay' && <IconArrowsExchange width={18} color="#FAB005" />}
                 </div>
-            )
-        })
+                <div className={styles.delete} onClick={() => deleteItem(item.id)}>
+                    <IconX color="#FA5252" width={17} height={17} />
+                </div>
+            </div>
+        ))
 
     return (
         <div className={styles.wrapper}>
@@ -103,7 +76,7 @@ const LoadFavorite = () => {
                 <span className={styles.error}>{!!error && error}</span>
             </h2>
             <div className={styles.favorite_wrapper}>{favorite_content}</div>
-            <button disabled={active_item_id === -1} className={styles.confirm_btn} onClick={() => setFavorite()}>
+            <button disabled={active_item_id === -1} className={styles.confirm_btn} onClick={setFavorite}>
                 Загрузить
             </button>
         </div>
