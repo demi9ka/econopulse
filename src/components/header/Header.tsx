a3b328c5df9cf08f600ed527bbdbfb2a7a5dda23
Demid <grazz1wmel@gmail.com>
2024-02-05 20:38:58 +0200

    add tg link

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,19 +3,26 @@ import { Link } from 'react-router-dom'
 import { useContext } from 'react'
 import { UserDataContext, IUserDataContext } from 'provider/UserProvider'
 import HeaderProfile from './header-profile/HeaderProfile'
+import { IconBrandTelegram } from '@tabler/icons-react'
+// import { IconBrandTelegram } from '@tabler/icons-react'
 const Header = () => {
     const { user_data } = useContext(UserDataContext) as IUserDataContext
     return (
         <header className={styles.header}>
             <div className={styles.wrapper}>
                 <div className={styles.content}>
-                    <Link className={styles.poster} to={'/'}>
-                        <img className={styles.logo} src="/logo.svg" alt="" />
-                        <h2>
-                            Econopulse<span className={styles.prefix}>.ru</span>{' '}
-                        </h2>
-                    </Link>
-                    <div>
+                    <div className={styles.nav}>
+                        <Link className={styles.poster} to={'/'}>
+                            <img className={styles.logo} src="/logo.svg" alt="" />
+                            <h2>
+                                Econopulse<span className={styles.prefix}>.ru</span>{' '}
+                            </h2>
+                        </Link>
+                    </div>
+                    <div className={styles.nav}>
+                        <a className={styles.tg_link} target="_blank" href="https://t.me/econopulse_ru">
+                            <IconBrandTelegram size={20} />
+                        </a>
                         {user_data === null && (
                             <>
                                 <Link className={`${styles.auth} ${styles.register}`} to={'/register'}>
