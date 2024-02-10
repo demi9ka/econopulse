e365a01404619f8e4d33791af6778e54b0df99a1
Demid <grazz1wmel@gmail.com>
2024-02-10 18:16:35 +0200

    edit chart style

--- src/components/home/chart/Chart.tsx
+++ src/components/home/chart/Chart.tsx
@@ -1,41 +1,43 @@
 import styles from './chart.module.css'
 import { Line } from 'react-chartjs-2'
-import { IChartDataContext, ChartDataContext } from 'provider/ChartDataProvider'
+import { IChartContext, ChartContext } from 'provider/ChartProvider'
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js'
 import annotationPlugin from 'chartjs-plugin-annotation'
 import { useContext } from 'react'
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, annotationPlugin)
-const options: any = {
-    responsive: true,
 
-    scales: {
-        y: {
-            beginAtZero: true,
-            position: 'right',
-        },
-    },
+const Chart = () => {
+    const { chart } = useContext(ChartContext) as IChartContext
+    const options: any = {
+        responsive: true,
 
-    plugins: {
-        legend: {
-            position: 'top' as const,
+        scales: {
+            y: {
+                // beginAtZero: true,
+                position: 'right',
+            },
         },
-        annotation: {
-            annotations: {
-                line1: {
-                    type: 'line',
-                    yMin: 0,
-                    yMax: 0,
-                    borderColor: '#868E96',
-                    borderWidth: 2,
+        borderWidth: 2,
+        pointBorderWidth: 1,
+        plugins: {
+            legend: {
+                position: 'top' as const,
+            },
+            annotation: {
+                annotations: {
+                    line1: {
+                        type: 'line',
+                        yMin: 0,
+                        yMax: 0,
+                        borderColor: '#868E96',
+                        borderWidth: 2,
+                    },
                 },
             },
         },
-    },
-}
-const Chart = () => {
-    const { chart_data } = useContext(ChartDataContext) as IChartDataContext
+    }
 
-    return <div className={`${styles.wrapper} ${chart_data && styles.active}`}>{chart_data && <Line options={options} data={chart_data} />}</div>
+    return <div className={`${styles.wrapper} ${chart && styles.active}`}>{chart && <Line options={options} data={chart} />}</div>
 }
 export default Chart
